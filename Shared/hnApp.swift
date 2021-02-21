4a3f83a5cb11dee3e84a179aeec34aebc8f6fe25
Preet Parekh <preetjdpdev@gmail.com>
2021-02-21 18:53:08 +0530

    Initial Commit

---
+++ Shared/hnApp.swift
@@ -0,0 +1,17 @@
+//
+//  hnApp.swift
+//  Shared
+//
+//  Created by Preet Parekh on 21/02/21.
+//
+
+import SwiftUI
+
+@main
+struct hnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
